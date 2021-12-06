0004f3009af43eba65355331e05d6589dbcc566f
H1mJT <92359223+H1mJT@users.noreply.github.com>
2021-12-06 08:57:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec  5 13:55:42 2021
@@ -10,15 +11,13 @@ app = Flask(__name__)
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
 import os
-import matplotlib.pyplot as plt
-from keras.preprocessing.image import load_img
 
 @app.route("/")
 def func():
     
     ENDPOINT = "https://assamesenumbers-prediction.cognitiveservices.azure.com/"
     prediction_key = "64f49a8cf19f44f293287d6cc477c401"
-    prediction_resource_id = "/subscriptions/adca6350-a85d-43d1-a0ed-be51a4efc45a/resourceGroups/Assamese-Numbers/providers/Microsoft.CognitiveServices/accounts/AssameseNumbers"
+   
     
     base_image_location = os.path.join (os.path.dirname(__file__), "Images")
     
@@ -35,10 +34,6 @@ def func():
        
     
        
-        plt.figure()
-        plt.clf()
-        plt.imshow(load_img("Images/Test/test_image1.jpg"), cmap = 'Greys')
-        plt.xlabel('Image1')
         print('Actual Number= ৬\nPrediction of Image1:')
         i=0
         # Display the results.
@@ -57,12 +52,7 @@ def func():
         results = predictor.classify_image(
             'e7ea3663-53a3-4644-b52a-1b271839d6dc', 'Iteration1', image_contents.read())
        
-    
-       
-        plt.figure()
-        plt.clf()
-        plt.imshow(load_img("Images/Test/test_image2.jpg"), cmap = 'Greys')
-        plt.xlabel('Image2')
+        
         print('Actual Number= ২\nPrediction of image2:')
         # Display the results.
         i=0
@@ -81,11 +71,7 @@ def func():
             'e7ea3663-53a3-4644-b52a-1b271839d6dc', 'Iteration1', image_contents.read())
        
     
-       
-        plt.figure()
-        plt.clf()
-        plt.imshow(load_img("Images/Test/test_image3.jpg"), cmap = 'Greys')
-        plt.xlabel('Image3')
+        
         print('Actual Number= ৯\nPrediction of Image 3:')
         # Display the results.
         i=0
@@ -99,5 +85,5 @@ def func():
                 
             else:
                 break
-        return "Image1: Prediction:",r,"Accuracy:",s,"Image2: Prediction:",q,"Accuracy:",w,"Image3: Prediction:",a,"Accuracy:",b
-print(func())
+        return f'<a href="https://github.com/H1mJT/Assamese-Numbers-Recogniser/blob/main/Images/Test/test_image1.jpg">Image1</a>: Prediction: {r}  Accuracy: {s}      <a href="https://github.com/H1mJT/Assamese-Numbers-Recogniser/blob/main/Images/Test/test_image2.jpg">Image2</a>: Prediction: {q}  Accuracy: {w}  <a href="https://github.com/H1mJT/Assamese-Numbers-Recogniser/blob/main/Images/Test/test_image3.jpg">Image3</a>: Prediction: {a}    Accuracy: {b}'
+
